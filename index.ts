0633328d7c8fd41d10ffeeb3ae8c057d28cc2bdb
Marlon Almino <alminomarlon@gmail.com>
2022-02-06 12:04:30 -0300

    interfaces

--- index.ts
+++ index.ts
@@ -1,3 +1,30 @@
+// Function
 function soma(a: number, b: number) {
   return a + b
+}
+
+// Interfaces
+interface IAnimal {
+  nome: string
+  tipo: 'terrestre' | 'aquático'
+  executarRugido(volumeEmDecibeis: number): void
+}
+
+interface IFelino extends IAnimal {
+  visaoNoturna: boolean
+}
+
+const animal: IAnimal = {
+  nome: 'Elefante',
+  tipo: 'terrestre',
+  executarRugido: (volumeEmDecibeis) => `${volumeEmDecibeis}dB`,
+}
+
+// animal.executarRugido('a') //vai dar erro pelo type
+
+const felino: IFelino = {
+  nome: 'Leão',
+  tipo: 'terrestre',
+  visaoNoturna: true,
+  executarRugido: (volumeEmDecibeis) => `${volumeEmDecibeis}`,
 }
